7e0dd844a88d528d2e0b72df17dfa561f0929110
4E6ynEJlb <152576482+4E6ynEJlb@users.noreply.github.com>
2025-02-17 20:09:11 +0200

    Update ClientException.cs

--- Domain/Models/ApplicationModels/Exceptions/ClientException.cs
+++ Domain/Models/ApplicationModels/Exceptions/ClientException.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Models.ApplicationModels.Exceptions
 {
-    public class ClientException : Exception
+    public abstract class ClientException : Exception
     {
         public HttpStatusCode Code;
         public new string? Message;
